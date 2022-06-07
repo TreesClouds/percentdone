7d094c26321de616025de199ed4ae217625b2385
Alexander DeRouen <34390214+TreesClouds@users.noreply.github.com>
2022-06-07 12:15:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function auto_refresh()
     let textBox = document.getElementById("update");
     let current_ms = Date.now();
     let start_ms = 1629124800000;
-    let end_ms = 1654714800000;
+    let end_ms = 1654707600000;
     let perc = ((current_ms - start_ms) / (end_ms - start_ms) * 100)
     if (perc > 100) {
         textBox.innerHTML = "100%";
