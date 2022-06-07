6f2f4db17e69d183e59804d12aee14c53d82df48
Alexander DeRouen <34390214+TreesClouds@users.noreply.github.com>
2022-06-06 17:38:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function auto_refresh()
     let textBox = document.getElementById("update");
     let current_ms = Date.now();
     let start_ms = 1629124800000;
-    let end_ms = 1654716600000;
+    let end_ms = 1654714800000;
     let perc = ((current_ms - start_ms) / (end_ms - start_ms) * 100)
     if (perc > 100) {
-        perc = "100";
+        textBox.innerHTML = "100%";
+        setTimeout(confetti.start, 1000);
+        setTimeout(confetti.stop, 5000);
     } else {
-        perc = perc.toFixed(10);
+        textBox.innerHTML = perc.toFixed(10) + "%";
+        setTimeout(auto_refresh, 1);
     }
-    textBox.innerHTML = perc + "%";
-    setTimeout(auto_refresh, 1);
 }
 
 auto_refresh()
